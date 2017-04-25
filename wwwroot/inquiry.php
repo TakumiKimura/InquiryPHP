9846549c9e14e3cd7f49af864cbf103adc918d93
TakumiKimura <kisaragi178@gmail.com>
2017-04-25 18:44:33 +0900

    2nd commit

---
+++ wwwroot/inquiry.php
@@ -0,0 +1,16 @@
+<?php
+
+?>
+<html>
+
+<body>
+	<form method="post" action="./inquiry_fin.php">
+		emailアドレス(*):<input type="text" name="email"><br>
+		名前:<input type="text" name="name"><br>
+		誕生日:<input type="text" name="birthday"><br>
+		問い合わせ内容<br><textarea name="body"></textarea><br>
+		<button>問い合わせる</button>
+	</form>
+</form>
+</body>
+</html>
